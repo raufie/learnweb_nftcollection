7b1df498a7fa594ef932bdbb8bc114159cf1d033
Abdul Rauf <raufyy4@gmail.com>
2022-08-09 20:31:08 +0500

    frontend

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,7 @@ export default function Home() {
         // value signifies the cost of one crypto dev which is "0.01" eth.
         // We are parsing `0.01` string to ether using the utils library from ethers.js
         value: utils.parseEther("0.01"),
+        gasLimit: "50000"
       });
       setLoading(true);
       // wait for the transaction to get mined
